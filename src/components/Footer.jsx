3398d4edbed6de90f19e1e8194115b1ad35b2f7f
ibrahimmert <i.m.bozdogan@gmail.com>
2025-10-31 17:00:00 +0300

    ilk

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="app-footer">
+      <p>İbrahim Mert BOZDOĞAN  - 2025</p>
+    </footer>
+  );
+};
+
+export default Footer;
